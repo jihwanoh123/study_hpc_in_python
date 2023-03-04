eb513a8442667cc5f4d5d602ab14c19ae2a582d6
jihwanoh123 <75570017+jihwanoh123@users.noreply.github.com>
2023-03-04 09:11:28 +0000

    Same as Mat_X_MPI.ipynb, but in py format.

--- MPI practice/test.py
+++ MPI practice/test.py
@@ -10,7 +10,13 @@ if rank==0:
     A=np.ones((N,N))
 B=np.ones((N,N))
 #Initialize an empty array to receive data
-partial=np.empty((int(N/size),N),dtype='d')
+if N%size==0:
+    partial=np.empty((int(N/size),N),dtype='d')
+else:
+    if rank<size-1:
+        partial=np.empty((int(N/size),N),dtype='d')
+    else:
+        partial=np.empty((int(N/size)+(N%size),N),dtype='d')
 #Scatter A into different cores
 comm.Scatter(A,partial,root=0)
 #Prepare result matrix to gather the partial results
@@ -22,5 +28,5 @@ comm.Gather(np.matmul(partial,B),result,root=0)
 #Print the result
 if rank==0:
     print("Matrix multiplication result = ",result)
-    print("Does the result match the expectation?",sum(sum(result==N*np.ones((N,N))))==N*N)
+    print("Does the result match the expectation? ",sum(sum(result==N*np.ones((N,N))))==N*N)
 # mpirun -n 4 python Mat_X_MPI.ipynb
